e3fa974308771ada55b726df6d73c2900d00e3b9
Patrick Shriwise <pshriwise@gmail.com>
2020-05-22 09:55:10 -0500

    Reorganizing so scenes are easier to swap.

--- tracer.cpp
+++ tracer.cpp
@@ -10,17 +10,17 @@
 #include "rand.h"
 #include "ray.h"
 
-Color ray_color(const Ray& r, const ObjectList& scene, int depth = 0) {
+Color ray_color(const Ray& r, const ObjectList& objects, int depth = 0) {
   Hit hit;
   if (depth > MAX_BOUNCE) {
     return Color(0,0,0);
   }
 
-  if (scene.hit(r, 0.001, INFTY, hit)) {
+  if (objects.hit(r, 0.001, INFTY, hit)) {
     Ray scattered;
     Color attenuation;
     if (hit.material_->scatter(r, hit, attenuation, scattered)) {
-      return attenuation * ray_color(scattered, scene, ++depth);
+      return attenuation * ray_color(scattered, objects, ++depth);
     }
     return Color(0.0, 0.0, 0.0);
   }
@@ -40,10 +40,17 @@ Color ray_color(const Ray& r, const ObjectList& scene, int depth = 0) {
   return (1.0-t)*Color(1.0, 1.0, 1.0) + t*Color(0.5, 0.7, 1.0);
 }
 
+struct Scene {
+  ObjectList objects_;
+  Camera camera_;
 
-ObjectList three_spheres() {
-    // Rendering objects
-  ObjectList scene;
+  inline Camera& camera() { return camera_; }
+  inline ObjectList& objects() { return objects_; }
+};
+
+Scene three_spheres() {
+  // Rendering objects
+  ObjectList objects;
   auto material1 = std::make_shared<Lambertian>(Color(0.1, 0.2, 0.5));
   auto material2 = std::make_shared<Lambertian>(Color(0.8 , 0.8, 0.0));
 
@@ -51,28 +58,55 @@ ObjectList three_spheres() {
   //auto material4 = std::make_shared<Metal>(Color(0.8, 0.8, 0.8), 1.0);
   auto material4 = std::make_shared<Dielectric>(1.5);
 
-  scene.add(std::make_shared<Sphere>(Point3(0, 0, -1), 0.5, material1));
-  scene.add(std::make_shared<Sphere>(Point3(0, -100.5, -1), 100, material2));
-  scene.add(std::make_shared<Sphere>(Point3(1.1, 0, -1), 0.5, material3));
-  scene.add(std::make_shared<Sphere>(Point3(-1.1, 0, -1), 0.5, material4));
+  objects.add(std::make_shared<Sphere>(Point3(0, 0, -1), 0.5, material1));
+  objects.add(std::make_shared<Sphere>(Point3(0, -100.5, -1), 100, material2));
+  objects.add(std::make_shared<Sphere>(Point3(1.1, 0, -1), 0.5, material3));
+  objects.add(std::make_shared<Sphere>(Point3(-1.1, 0, -1), 0.5, material4));
 
-  return scene;
+  // Setup camera
+  Point3 camera_position{3, 3, 2};
+  Point3 camera_target{0, 0, -1};
+  double field_of_view = 20;
+  double aperture = 2.0;
+
+  Camera camera(camera_position,
+                camera_target,
+                Point3(0, 1, 0),
+                field_of_view,
+                ASPECT_RATIO,
+                aperture,
+                (camera_position - camera_target).length());
+
+  return {objects, camera};
 }
 
-ObjectList red_blue() {
-  ObjectList scene;
+Scene red_blue() {
+  ObjectList objects;
 
   double r = cos(PI/4);
-  scene.add(std::make_shared<Sphere>(Point3(-r, 0, -1),
+  objects.add(std::make_shared<Sphere>(Point3(-r, 0, -1),
             r,
             std::make_shared<Lambertian>(Color(0,0,1))));
-  scene.add(std::make_shared<Sphere>(Point3(r, 0, -1),
+  objects.add(std::make_shared<Sphere>(Point3(r, 0, -1),
             r,
             std::make_shared<Lambertian>(Color(1, 0, 0))));
 
-  return scene;
-}
+    // Setup camera
+  Point3 camera_position{0, 0, 0};
+  Point3 camera_target{0, 0, -1};
+  double field_of_view = 90;
+  double aperture = 0.0; // no defocus blur
 
+  Camera camera(camera_position,
+                camera_target,
+                Point3(0, 1, 0),
+                field_of_view,
+                ASPECT_RATIO,
+                aperture,
+                (camera_position - camera_target).length());
+
+  return {objects, camera};
+}
 
 int main() {
 
@@ -82,18 +116,7 @@ int main() {
   const int image_height = static_cast<int>(image_width / ASPECT_RATIO);
   outfile << "P3\n" << image_width << " " << image_height << "\n" << IRGB_MAX << "\n";
 
-  auto scene = three_spheres();
-
-  Point3 camera_position{3, 3, 2};
-  Point3 camera_target{0, 0, -1};
-
-  Camera camera(camera_position,
-                camera_target,
-                Point3(0, 1, 0),
-                20,
-                ASPECT_RATIO,
-                2.0,
-                (camera_position - camera_target).length());
+  Scene scene = red_blue();
 
   for (int j = image_height - 1; j >= 0; --j) {
     std::cerr << "\rScanlines remaining: " << j << " " << std::flush;
@@ -102,8 +125,8 @@ int main() {
       for (int s = 0; s < SAMPLES_PER_PIXEL; ++s) {
         double u = (i + nrand()) / (image_width - 1);
         double v = (j + nrand()) / (image_height - 1);
-        Ray r = camera.get_ray(u, v);
-        pixel_color += ray_color(r, scene);
+        Ray r = scene.camera().get_ray(u, v);
+        pixel_color += ray_color(r, scene.objects());
       }
       write_color(outfile, pixel_color, SAMPLES_PER_PIXEL);
     }

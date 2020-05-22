080b5898126d57a71f32dbd584b31b3642d52d19
Patrick Shriwise <pshriwise@gmail.com>
2020-05-21 20:42:28 -0500

    Adding an object class. Establishing our sad object normal convention.

--- geom.cpp
+++ geom.cpp
@@ -30,16 +30,33 @@ bool Sphere::hit(const Ray& r, double t_min, double t_max, Hit& rec) const {
     if (t > t_min && t < t_max) {
       rec.t_ = t;
       rec.p_ = r.at(t);
-      rec.n_ = (rec.p_ - center_) / radius_;
+      auto outward_norm = (rec.p_ - center_) / radius_;
+      rec.set_face_normal(r, outward_norm);
       return true;
     }
     t = (-half_b + root) / a;
     if (t > t_min && t < t_max) {
       rec.t_ = t;
       rec.p_ = r.at(t);
-      rec.n_ = (rec.p_ - center_) / radius_;
+      auto outward_norm = (rec.p_ - center_) / radius_;
+      rec.set_face_normal(r, outward_norm);
       return true;
     }
   }
   return false;
+}
+
+bool ObjectList::hit(const Ray& r, double t_min, double t_max, Hit& rec) const {
+  Hit temp_hit;
+  bool hit_something = false;
+  double t_curr = t_max;
+
+  for (const auto& object : objects_) {
+    if (object->hit(r, t_min, t_curr, temp_hit)) {
+      hit_something = true;
+      t_curr = temp_hit.t_;
+      rec = temp_hit;
+    }
+  }
+  return hit_something;
 }
--- geom.h
+++ geom.h
@@ -4,11 +4,21 @@
 #ifndef GEOM_H
 #define GEOM_H
 
+#include <memory>
+#include <vector>
+
 #include "ray.h"
 
 struct Hit {
   Point3 p_;
   Vec3 n_;
+  bool front_face_;
+
+  inline void set_face_normal(const Ray& r, const Vec3& outward_normal) {
+    front_face_ = dot(r.direction(), outward_normal) < 0.0;
+    n_ = front_face_ ? outward_normal : -outward_normal;
+  }
+
   double t_;
 };
 
@@ -16,6 +26,24 @@ class Object {
 public:
   virtual bool hit(const Ray& r, double t_min, double t_max, Hit& rec) const = 0;
 };
+
+class ObjectList : public Object {
+
+public:
+  // Constructors
+  ObjectList() {};
+  ObjectList(std::shared_ptr<Object> object) { add(object); }
+
+  // Methods
+  void clear() { objects_.clear(); }
+  void add(std::shared_ptr<Object> object) { objects_.push_back(object); }
+
+  virtual bool hit(const Ray& r, double t_min, double t_max, Hit& rec) const;
+
+  // Data Members
+  std::vector<std::shared_ptr<Object>> objects_;
+};
+
 
 class Sphere : public Object {
 public:

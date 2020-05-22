67517767c1bf7e7dfad672e4d0384a94fb9d2e4b
Patrick Shriwise <pshriwise@gmail.com>
2020-05-21 20:53:17 -0500

    Allowing multiple objects in a scene.

--- tracer.cpp
+++ tracer.cpp
@@ -1,30 +1,28 @@
 
 #include <iostream>
+#include <memory>
 
 #include "color.h"
 #include "constants.h"
 #include "geom.h"
 #include "ray.h"
 
-Color ray_color(const Ray& r) {
-  double t;
-
-  Sphere s({0, 0, -1}, 0.5);
+Color ray_color(const Ray& r, const ObjectList& scene) {
   Hit hit;
-
-  if (s.hit(r, 0.0, INFTY, hit)) {
+  if (scene.hit(r, 0.0, INFTY, hit)) {
       // compute the location of the hit
       auto& normal = hit.n_;
-      return 0.5 * Color(normal.x()+1, normal.y()+1, normal.z()+1);
+      return 0.5 * (hit.n_ + Color(1.0, 1.0, 1.0));
   }
+
   Vec3 unit_direction = unit_vector(r.direction());
-  t = 0.5*(unit_direction.y() + 1.0);
+  double t = 0.5*(unit_direction.y() + 1.0);
   return (1.0-t)*Color(1.0, 1.0, 1.0) + t*Color(0.5, 0.7, 1.0);
 }
 
 int main() {
 
-  const int image_width = 384;
+  const int image_width = 500;
   const int image_height = static_cast<int>(image_width / ASPECT_RATIO);
   std::cout << "P3\n" << image_width << " " << image_height << "\n" << IRGB_MAX << "\n";
 
@@ -41,13 +39,17 @@ int main() {
   // lower left corner
   auto llc = origin - horizontal/2 - vertical/2 - Vec3(0, 0, focal_length);
 
+  ObjectList scene;
+  scene.add(std::make_shared<Sphere>(Point3(0, 0, -1), 0.5));
+  scene.add(std::make_shared<Sphere>(Point3(0, -100.5, -1), 100));
+
   for (int j = image_height - 1; j >= 0; --j) {
     std::cerr << "\rScanlines remaining: " << j << std::flush;
     for (int i = 0; i < image_width; ++i) {
       double u = double(i) / (image_width - 1);
       double v = double(j) / (image_height - 1);
       Ray r{origin, llc + u * horizontal + v * vertical - origin};
-      Color pixel_color = ray_color(r);
+      Color pixel_color = ray_color(r, scene);
       write_color(std::cout, pixel_color);
     }
   }

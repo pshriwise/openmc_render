d9e8a61f485800265f8e42097389f2f419173460
Patrick Shriwise <pshriwise@gmail.com>
2020-05-27 21:56:04 -0500

    Modifying model scripts a bit - no materials should be needed.

--- models/demo_model.py
+++ models/demo_model.py
@@ -1,14 +1,7 @@
-
-
 import openmc
 
 
-pwr_ex = openmc.examples.pwr_pin_cell()
-
-mats = pwr_ex.geometry.get_all_materials()
-
-model = openmc.model.Model()
-
+# Geometry
 ground_sphere = openmc.Sphere(0, -100.5, 0, 100, boundary_type='vacuum')
 
 cyl = openmc.YCylinder(0, -1, 0.5, boundary_type='vacuum')
@@ -16,22 +9,25 @@ ymax = openmc.YPlane(1.0, boundary_type='vacuum')
 ymin = openmc.YPlane(0.3, boundary_type='vacuum')
 top_cap = openmc.Sphere(0, 1.0, -1, 0.5, boundary_type='vacuum')
 bottom_cap = openmc.Sphere(0, 0.3, -1, 0.5, boundary_type='vacuum')
-ground_cell = openmc.Cell(fill=mats[1], region=-ground_sphere)
+ground_cell = openmc.Cell(region=-ground_sphere)
 
-marble = openmc.Cell(fill=mats[3], region=((-cyl & -ymax & +ymin) | -top_cap | -bottom_cap))
+pill = openmc.Cell(region=((-cyl & -ymax & +ymin) | -top_cap | -bottom_cap))
 another_sphere = openmc.Sphere(-1.5, 0.5, -1.0, 0.5, boundary_type='vacuum')
-
-another_sphere_cell = openmc.Cell(fill=mats[1], region=-another_sphere)
+another_sphere_cell = openmc.Cell(region=-another_sphere)
 
 ycone = openmc.YCone(1.5, 1.0, -1, 0.5, boundary_type='vacuum')
+cone_cell = openmc.Cell(region=(-ycone & -ymax & +ymin))
 
-cone = openmc.Cell(fill=mats[1], region=(-ycone & -ymax & +ymin))
+geometry = openmc.Geometry([ground_cell, pill, another_sphere_cell, cone_cell])
+geometry.export_to_xml()
 
-model.geometry = openmc.Geometry([ground_cell, marble, another_sphere_cell, cone])
-model.materials = [mats[1], mats[3]]
+# Placeholder materials file
+openmc.Materials().export_to_xml()
 
-model.settings.particles = 500
-model.settings.batches = 10
-model.settings.inactive = 1
+# Minimal settings
+settings = openmc.Settings()
+settings.particles = 500
+settings.batches = 10
+settings.inactive = 1
 
-model.export_to_xml()
+settings.export_to_xml()
--- models/openmc_book_cover_model.py
+++ models/openmc_book_cover_model.py
@@ -4,18 +4,15 @@ import openmc
 import numpy as np
 from random import random
 
-mat = openmc.Material()
-mat.add_nuclide('U235', 1.0)
-mat.set_density('g/cc', 11.0)
-
 ground_sphere = openmc.Sphere(0, -1000, 0, 1000, boundary_type='vacuum')
 
-ground = openmc.Cell(fill=mat, region=-ground_sphere, name="ground")
+ground = openmc.Cell(region=-ground_sphere, name="ground")
 
 spheres = [ground]
 
 middle = np.asarray((0, 0.2, 0))
 
+# randomly placed spherical cells
 for a in range(-11, 11):
     for b in range(-11, 11):
         center = np.asarray((a + 0.9 * random(), 0.2, b + 0.9 * random()))
@@ -33,29 +30,32 @@ for a in range(-11, 11):
         else:
             cell_name = "Dielectric"
 
-        spheres.append(openmc.Cell(fill=mat, region=-sphere, name=cell_name))
+        spheres.append(openmc.Cell(region=-sphere, name=cell_name))
 
 # setup the three larger spheres
 sphere1 = openmc.Sphere(0, 1, 0, 1.0, boundary_type='vacuum')
-spheres.append(openmc.Cell(fill=mat, region=-sphere1, name='Dielectric'))
+spheres.append(openmc.Cell(region=-sphere1, name='Dielectric'))
 
 sphere2 = openmc.Sphere(-4, 1, 0, 1.0, boundary_type='vacuum')
-spheres.append(openmc.Cell(fill=mat, region=-sphere2, name='Lambertian'))
+spheres.append(openmc.Cell(region=-sphere2, name='Lambertian'))
 
 sphere3 = openmc.Sphere(4, 1, 0, 1.0, boundary_type='vacuum')
-spheres.append(openmc.Cell(fill=mat, region=-sphere3, name='Metal'))
+spheres.append(openmc.Cell(region=-sphere3, name='Metal'))
 
 model = openmc.model.Model()
 
-model.geometry = openmc.Geometry(spheres)
+geometry = openmc.Geometry(spheres)
+geometry.export_to_xml()
 
-model.materials = [mat]
+# Placeholder materials file
+openmc.Materials().export_to_xml()
 
-model.settings.particles = 500
-model.settings.batches = 10
-model.settings.inactive = 1
+settings = openmc.Settings()
+settings.particles = 500
+settings.batches = 10
+settings.inactive = 1
 
-model.export_to_xml()
+settings.export_to_xml()
 
 
 
